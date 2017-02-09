1311b2c2571fb979bf9ae5e86b481fd512dcd3b0
Willard <willard.torres1008@gmail.com>
2017-02-09 09:58:28 +0800

    Update descriptions

--- gamelist.js
+++ gamelist.js
@@ -2,7 +2,7 @@ gamelist = [
 	{
 		"title": "shoot o' clock",
 		"img": "shootoclock",
-		"desc": "shoot o' clock is an action arcade game (using the terms roughly). Made for Ludum Dare 27, with the theme of \"10 seconds\".",
+		"desc": "An action arcade game (using the terms roughly). Made for Ludum Dare 27, with the theme of \"10 seconds\".",
 		"links": {
 			"GameJolt": "http://gamejolt.com/games/arcade/shoot-o-clock/18337/"
 		}
@@ -10,7 +10,7 @@ gamelist = [
 	{
 		"title": "Primary Run",
 		"img": "primaryrun",
-		"desc": "Primary Run is a third person puzzle game with the goal of getting everyone to the exit. Made for Global Game Jam 2014.",
+		"desc": "A third person puzzle game with the goal of getting everyone to the exit. Made for Global Game Jam 2014.",
 		"links": {
 			"Global Game Jam Page": "http://globalgamejam.org/2014/games/primary-run"
 		}
@@ -18,7 +18,7 @@ gamelist = [
 	{
 		"title": "Logic Invaders",
 		"img": "logicinvaders",
-		"desc": "Logic Invaders is a Space Invader clone using the mechanics of logic gates (AND, OR, etc.). Made for a school project.",
+		"desc": "A Space Invader clone using the mechanics of logic gates (AND, OR, etc.). Made for a school project.",
 		"links": {
 			"Kongregate": "http://www.kongregate.com/games/will1008/logic-invaders"
 		}
@@ -26,7 +26,7 @@ gamelist = [
 	{
 		"title": "Not Quite Pong",
 		"img": "notquitepong",
-		"desc": "Not Quite Pong is a pong clone, but with Flappy Bird-like controls. Made for FlappyJam 2014, hosted by @somepx.",
+		"desc": "A pong clone, but with Flappy Bird-like controls. Made for FlappyJam 2014, hosted by @somepx.",
 		"desc_links": {
 			"@somepx": "http://twitter.com/somepx"
 		},
@@ -38,7 +38,7 @@ gamelist = [
 	{
 		"title": "AXE Punk",
 		"img": "axepunk",
-		"desc": "AXE Punk is an action platformer. Made for CyberpunkJam 2014, hosted by @deviever.",
+		"desc": "An action platformer. Made for CyberpunkJam 2014, hosted by @deviever.",
 		"desc_links": {
 			"@deviever": "http://twitter.com/deviever"
 		},
@@ -49,7 +49,7 @@ gamelist = [
 	{
 		"title": "Awake",
 		"img": "awake",
-		"desc": "Awake is a short adventure platformer about disillusionment. Made for LowRezJam 2014, hosted by @deviever.",
+		"desc": "A short adventure platformer about disillusionment. Made for LowRezJam 2014, hosted by @deviever.",
 		"desc_links": {
 			"@deviever": "http://twitter.com/deviever"
 		},
@@ -60,7 +60,7 @@ gamelist = [
 	{
 		"title": "Radial Blur",
 		"img": "radialblur",
-		"desc": "Radial Blur is an action shooter. Made for a local \"One Year Game Challenge++\" 2013-2014, hosted by IGDA Manila.",
+		"desc": "A wave-based shooter. Made for a local \"One Year Game Challenge++\" 2013-2014, hosted by IGDA Manila.",
 		"links": {
 			"Google Play": "https://play.google.com/store/apps/details?id=com.anheu.rblur",
 			"itch.io": "http://anheurystics.itch.io/radial-blur"
@@ -69,7 +69,7 @@ gamelist = [
 	{
 		"title": "Alone Together",
 		"img": "alonetogether",
-		"desc": "Alone Together is a minimalist puzzle platformer. Made for Ludum Dare 30, with the theme of \"Connected Worlds\".",
+		"desc": "A minimalist puzzle platformer. Made for Ludum Dare 30, with the theme of \"Connected Worlds\".",
 		"links": {
 			"itch.io": "http://anheurystics.itch.io/alone-together",
 			"GameJolt": "http://gamejolt.com/games/puzzle/alone-together/33161/"
@@ -78,7 +78,7 @@ gamelist = [
 	{
 		"title": "Base 10-2",
 		"img": "base10-2",
-		"desc": "Base 10-2 is a simple arithmetic game. Made for the IndieQuilt Jam, hosted by @Erichermit.",
+		"desc": "A time-based arithmetic quiz game. Made for the IndieQuilt Jam, hosted by @Erichermit.",
 		"desc_links": {
 			"@Erichermit": "http://twitter.com/erichermit"
 		},
@@ -90,7 +90,7 @@ gamelist = [
 	{
 		"title": "End Game+",
 		"img": "endgameplus",
-		"desc": "End Game+ is a game that deals with the idea of a life after the boss fight. Made for Global Game Jam 2015",
+		"desc": "A game that deals with the idea of a life after the boss fight. Made for Global Game Jam 2015",
 		"links": {
 			"Global Game Jam Page": "http://globalgamejam.org/2015/games/end-game"
 		}
@@ -98,7 +98,7 @@ gamelist = [
 	{
 		"title": "Baker's Dozen",
 		"img": "bakersdozen",
-		"desc": "Baker's Dozen is a top down action shooter about shooting cookies at robots. Made for Ludum Dare 32, with the theme of \"An Unconventional Weapon\".",
+		"desc": "A top down action shooter about shooting cookies at robots. Made for Ludum Dare 32, with the theme of \"An Unconventional Weapon\".",
 		"links": {
 			"Ludum Dare page": "http://ludumdare.com/compo/ludum-dare-32/?action=preview&uid=26198"
 		}
@@ -106,7 +106,7 @@ gamelist = [
 	{
 		"title": "Voxel Populus",
 		"img": "voxelpopulus",
-		"desc": "Voxel Populus is a block-building sandbox game made for a local hackathon, YouthHack Code Weekend",
+		"desc": "A block-building sandbox game made for a local hackathon, YouthHack Code Weekend",
 		"links": {
 			"GitHub": "http://github.com/Anheurystics/VoxelPopulus"
 		}
@@ -114,7 +114,7 @@ gamelist = [
 	{
 		"title": "Our Dark Lord (is Better than Yours)",
 		"img": "ourdarklord",
-		"desc": "Our Dark Lord (is Better than Yours) is a competitive PvP game, in which cults race to summon their demon before everyone else. Made for Global Gam Jam 2016.",
+		"desc": "A competitive PvP first-person game, in which cults race to summon their demon before everyone else. Made for Global Gam Jam 2016.",
 		"links": {
 			"Global Game Jam Page": "http://globalgamejam.org/2016/games/our-dark-lord-better-yours"
 		}
